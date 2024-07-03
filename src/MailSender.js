66c74cb24ab0a551d9722baf21ef72557f777928
programmermedical <programmermedical@gmail.com>
2024-07-03 13:10:06 +0700

    update env variable

--- src/MailSender.js
+++ src/MailSender.js
@@ -3,11 +3,11 @@ const nodemailer = require('nodemailer');
 class MailSender {
   constructor() {
     this._transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
-      port: process.env.MAIL_PORT,
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
       auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASSWORD,
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
       },
     });
   }
